10a363e773114f0af074e6ea6f504d0450eaeb59
Bat-Erdene912 <erdenee0912@gmail.com>
2025-03-18 14:44:23 +0800

    Update IntQueueTest.java

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -10,45 +10,83 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+/**
+ * @author Alex Lockwood, George Guo, Terry Li
+ */
 public class IntQueueTest {
 
+  /**
+ * This object constraction.
+
+ */
     private IntQueue mQueue;
+    /**
+     * List.
+     */
     private List<Integer> testList;
 
+    /**
+     * Called before each test.
+     */
     @Before
     public void setUp() {
-        // Toggle between LinkedIntQueue and ArrayIntQueue
-        mQueue = new LinkedIntQueue();
-        // mQueue = new ArrayIntQueue();
-        testList = new ArrayList<>(List.of(1, 2, 3));
-    }
+        // comment/uncomment these lines to test each class
+        // mQueue = new LinkedIntQueue();
+        mQueue = new ArrayIntQueue();
 
+        final int num1 = 1;
+        final int num2 = 2;
+        final int num3 = 3;
+        testList = new ArrayList<>(List.of(num1, num2, num3));
+    }
+    /**
+     * Testing qeue is Empty.
+     */
     @Test
     public void testIsEmpty() {
+        // This is an example unit test
         assertTrue(mQueue.isEmpty());
     }
 
+    /**
+     * Testing qeue not empty.
+     */
     @Test
     public void testNotEmpty() {
-        mQueue.enqueue(1);
-        assertFalse(mQueue.isEmpty());
+        for (int i = 0; i < testList.size(); i++) {
+            mQueue.enqueue(testList.get(i));
+            assertFalse(mQueue.isEmpty());
+        }
     }
-
+    /**
+     * Testing peek is empty.
+     */
     @Test
     public void testPeekEmptyQueue() {
-        assertNull(mQueue.peek());
+       assertNull(mQueue.peek());
     }
 
+    /**
+     * Testing peek is not empty.
+     */
     @Test
     public void testPeekNoEmptyQueue() {
-        mQueue.enqueue(5);
-        assertEquals(Integer.valueOf(5), mQueue.peek());
+        for (int i = 0; i < testList.size(); i++) {
+            mQueue.enqueue(testList.get(i));
+            assertEquals(testList.get(0), mQueue.peek());
+        }
     }
 
+    /**
+     * Testing giving value in qeue.
+     */
     @Test
     public void testEnqueue() {
+        // This is an example unit test
         for (int i = 0; i < testList.size(); i++) {
             mQueue.enqueue(testList.get(i));
             assertEquals(testList.get(0), mQueue.peek());
@@ -56,43 +94,58 @@ public class IntQueueTest {
         }
     }
 
+    /**
+     * Testing take value in qeue.
+     */
     @Test
     public void testDequeue() {
-        mQueue.enqueue(5);
-        mQueue.enqueue(10);
-        assertEquals(Integer.valueOf(5), mQueue.dequeue());
-        assertEquals(Integer.valueOf(10), mQueue.dequeue());
-        assertTrue(mQueue.isEmpty());
+        for (int i = 0; i < testList.size(); i++) {
+            mQueue.enqueue(testList.get(i));
+            assertEquals(testList.get(i), mQueue.dequeue());
+        }
     }
 
+    /**
+     * Testing.
+     */
     @Test
-    public void testDequeueEmptyQueue() {
+    public void testClear() {
+        for (int i = 0; i < testList.size(); i++) {
+            mQueue.enqueue(testList.get(i));
+        }
+        mQueue.clear();
+
+        assertTrue(mQueue.isEmpty());
+        assertEquals(0, mQueue.size());
+    }
+
+    /**
+     * testing size.
+     */
+    @Test
+    public void testEnsureCapacity() {
+    for (int i = 0; i < 20; i++) {
+        mQueue.enqueue(i);
+
+    }
+    assertEquals(20, mQueue.size());
+    assertFalse(mQueue.isEmpty());
+}
+
+    /**
+     * test.
+     */
+    @Test
+    public void testEmptyDeqeue() {
         assertNull(mQueue.dequeue());
     }
 
-    @Test
-    public void testSize() {
-        assertEquals(0, mQueue.size());
-        mQueue.enqueue(1);
-        mQueue.enqueue(2);
-        assertEquals(2, mQueue.size());
-        mQueue.dequeue();
-        assertEquals(1, mQueue.size());
-    }
-
-    @Test
-    public void testOrderPreserved() {
-        for (int num : testList) {
-            mQueue.enqueue(num);
-        }
-        for (int num : testList) {
-            assertEquals(Integer.valueOf(num), mQueue.dequeue());
-        }
-        assertTrue(mQueue.isEmpty());
-    }
-
+    /**
+     * @throws IOException
+     */
     @Test
     public void testContent() throws IOException {
+        // This is an example unit test
         InputStream in = new FileInputStream("src/test/resources/data.txt");
         try (Scanner scanner = new Scanner(in)) {
             scanner.useDelimiter("\\s*fish\\s*");
@@ -101,11 +154,12 @@ public class IntQueueTest {
             while (scanner.hasNextInt()) {
                 int input = scanner.nextInt();
                 correctResult.add(input);
+                System.out.println("enqueue: " + input);
                 mQueue.enqueue(input);
             }
 
             for (Integer result : correctResult) {
-                assertEquals(result, mQueue.dequeue());
+                assertEquals(mQueue.dequeue(), result);
             }
         }
     }
